9f4d0f2c428b2257eb9001ab8c8b328c6fa9298a
NehalJawi <nehal.jawi@gmail.com>
2023-04-04 01:27:42 +0300

    add function documentation

--- function_Lab.py
+++ function_Lab.py
@@ -3,14 +3,17 @@ Create a function that takes 1 parameter of type int ,
 then it prints out the result formatted like 
 the following patter (if we give it 5 for example):
 '''
-''''
-number = int(input("Enter number:1"))
-while  number <= 5:
-    print(number)
+def pattren(rows:int):
     '''
+            function used to print number in Descending order with triangle pattren
+    '''
+    for i in range(0, rows + 1):  
+        # inner loop for decrement in i values  
+        for j in range(rows - i, 0, -1):  
+            print(j, end=' ')  
+        print("\n")
+
 rows = int(input("Enter number of rows:"))
-for i in range(0, rows + 1):  
-    # inner loop for decrement in i values  
-    for j in range(rows - i, 0, -1):  
-        print(j, end=' ')  
-    print("\n")
+pattren(rows)
+print(pattren.__doc__)
+
